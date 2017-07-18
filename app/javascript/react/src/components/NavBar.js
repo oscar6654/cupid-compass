1fae9896c8d1f3d3950b964b6a38495b4bf098f8
Peter Nelson <peternelson@Peters-MacBook-Air.local>
2017-07-18 17:21:59 -0400

    React-Router installed, hooked-up, and running as expected

--- app/javascript/react/src/components/NavBar.js
+++ app/javascript/react/src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 const NavBar = (props) =>{
 
   return(
-    <h1> Hello World</h1>
+    <h1> Hello from inside navbar</h1>
   )
 }
 
--- app/javascript/react/src/containers/App.js
+++ app/javascript/react/src/containers/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {Router,browserHistory,Route} from 'react-router';
+// import {Router,browserHistory,Route,IndexRoute} from 'react-router';
 import NavBar from '../components/NavBar'
+import { Route, Switch } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory'
+import { BrowserRouter } from 'react-router-dom'
 
-// <Router history = {browserHistory}>
-// <Route path='/'>
-// <IndexRoute component={NavBar}/>
-// </Route>
-// </Router>
+const history = createBrowserHistory();
+
 const App = props =>{
   return(
+    <div>
     <h1> Hello from inside the computer</h1>
+    <BrowserRouter history={history}>
+      <Switch>
+        <Route exact path='/' component={NavBar} />
+      </Switch>
+    </BrowserRouter>
+    </div>
   )
-
 }
 
 export default App
