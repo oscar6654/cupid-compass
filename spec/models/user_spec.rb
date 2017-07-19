73ade00f9f88cfb1231f474f418104acaf4f51cc
Peter Nelson <peternelson@Peters-MacBook-Air.local>
2017-07-19 09:52:38 -0400

    added additional config for hound / rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,14 +23,4 @@ RSpec.describe User, :type => :model do
     subject.last_name = nil
     expect(subject).to_not be_valid
   end
-
-  # it "is not valid without an email" do
-  #   subject.email = nil
-  #   expect(subject).to_not be_valid
-  # end
-  #
-  # it "is not valid without a password" do
-  #   subject.password = nil
-  #   expect(subject).to_not be_valid
-  # end
 end
