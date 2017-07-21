f5430e6fa973459e91a5bf661d0554be3ecf2a5c
Peter Nelson <peternelson@Peters-MacBook-Air.local>
2017-07-21 16:14:01 -0400

    added show page in react

---
+++ app/javascript/react/src/containers/Locations.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import LocationForm from './LocationForm';
+import { Link } from 'react-router-dom';
+
+class Locations extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      locations: [],
+      formShow: false
+    }
+
+    this.handleFormShow = this.handleFormShow.bind(this)
+    this.createLocation = this.createLocation.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/api/v1/locations')
+    .then(response => {
+      return response.json()
+    })
+    .then(body => {
+      this.setState({ locations: body })
+    })
+  }
+
+  createLocation(payload) {
+    console.log(`payload: ${payload}`)
+
+    fetch('/api/v1/locations', {
+      method: 'POST',
+      credentials: "same-origin",
+      body: JSON.stringify(payload)
+    }).then(response => {
+      let body = response.json()
+      return body;
+    }).then(body => {
+      this.setState({formShow:false})
+      let newLocations = this.state.locations.slice()
+      newLocations.unshift(body)
+      this.setState({ locations: newLocations })
+    })
+  }
+
+  handleFormShow(event) {
+    event.preventDefault()
+    this.setState({formShow: !this.state.formShow})
+  }
+
+  render() {
+    let locations = this.state.locations.map( (location, index) => {
+      return(
+
+        <ul key={index}>
+          <Link to={`/locations/${location.id}`}>{location.name}</Link>
+        </ul>
+      )
+    })
+
+    let buttonText;
+    let form = ""
+
+    if (this.state.formShow){
+      form = <LocationForm createLocation={this.createLocation} />
+
+      buttonText = "Hide Form"
+    } else {
+      buttonText = "Add Location"
+    }
+
+    return(
+
+      <div>
+        <button type="button" onClick={this.handleFormShow}>{buttonText}</button>
+        {form}
+        {locations}
+      </div>
+    )
+  }
+}
+
+export default Locations
