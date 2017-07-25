e6d6d9bea86ffd74c4eb85c890e9c652ad4dcb70
Peter Nelson <peternelson@Peters-MacBook-Air.local>
2017-07-25 16:41:26 -0400

    added error handler function and changed the Location model to have a url regex validator

--- app/javascript/react/src/containers/LocationForm.js
+++ app/javascript/react/src/containers/LocationForm.js
@@ -13,16 +13,20 @@ class LocationForm extends Component {
       city: "",
       state: "",
       zip: "",
-      url: ""
+      url: "",
+      errorList: []
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleClearForm = this.handleClearForm.bind(this);
+    this.errorHandler = this.errorHandler.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value })
+    if (!this.errorHandler(event.target.name, event.target.value, errorDictionary)) {
+      this.setState({ [event.target.name]: event.target.value })
+    }
   }
 
   handleFormSubmit(event) {
@@ -53,11 +57,66 @@ class LocationForm extends Component {
       city: "",
       state: "",
       zip: "",
-      url: ""
+      url: "",
+      errorList: []
     })
   }
 
+  errorHandler(fieldCategory, value, errorDictionary) {
+    if (errorDictionary.fieldCategory.conditional(value)) {
+      let errorsArray = [...this.state.errorList, errorDictionary.fieldCategory.message]
+      this.setState({ errorList: errorsArray });
+    }
+    else {
+      if (errorArray.includes(message)) {
+        let newError = this.state.errorList.filter((error) => {
+          return error !== errorDictionary.fieldCategory.message
+        })
+        this.setState({ errorList: newError })
+      }
+    }
+  }
+
   render() {
+    let errorDictionary = {
+      name: {
+        conditional: (value) => {
+          return (value.length >= 2)
+        },
+      message: "name must be a minimum of 2 characters in length"
+      },
+      description: {
+        conditional: () => {
+          return (50 <= value.length && value.length <= 1000)
+        },
+        message: "description must be a minimum of 50 characters and no more than 1000 characters in length"
+      },
+      address: {
+        conditional: () => {
+          return (value.length >= 2)
+        },
+        message: "address must be a minimum of 2 characters in length"
+      },
+      city: {
+        conditional: () => {
+          return (value.length >= 2)
+        },
+        message: "city must be a minimum of 2 characters in length"
+      },
+      zip: {
+        conditional: () => {
+          return (value.length === 5 && value.match('/^[0-9]{5}$/'))
+        },
+        message: "zip code must be exactly 5 numbers"
+      },
+      url: {
+        conditional: () => {
+          return value.match('/^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix')
+        },
+        message: "url must be a valid url"
+      }
+    }
+
     let fieldCategories = Object.keys(this.state).map( (key) => {
       return key;
     } ); //array of field categories
@@ -88,8 +147,17 @@ class LocationForm extends Component {
     }
     })
 
+    let errors = this.state.errorList.map(error => {
+      return(
+        <h5 key={error}>
+        {error}
+        </h5>
+      )
+    })
+
     return(
       <div>
+      {errors}
       <form className="form" onSubmit={this.handleFormSubmit}>
         {textFields}
         <input type="submit" className="btn waves-effect waves-light" name="Submit"/>
