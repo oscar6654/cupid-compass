55bb2ea296b75a9673b2f28544460318b4bfeead
Jared Shulman <JaredCapImpact@Jareds-MacBook-Air-3.local>
2017-07-26 11:25:26 -0400

    Added Picture and Name to review

--- app/javascript/react/src/containers/Review.js
+++ app/javascript/react/src/containers/Review.js
@@ -17,15 +17,32 @@ class Review extends Component {
   }
 
   render() {
+
+    let user_name;
+    let user_image = ""
+
+    if (this.props.review.user) {
+        user_name = this.props.review.user.first_name
+        user_image = this.props.review.user.profile_photo.url
+        if (user_image === null) {
+          user_image = "http://abcnews.go.com/images/Entertainment/ht_randy_jackson_idol_nt_130409_wmain.jpg"
+        }
+    }
     return(
       <div>
         <div className="col s12 m6">
           <div className="card horizontal">
             <div className="card-content black-text">
-              <span className="card-title">User Name</span>
+              <div className="chip">
+                <img src={user_image}
+                alt="USERIMAGE"
+                height="60" width="60"
+                className= "circle" />
+                {user_name}
+              </div>
                 Votes:{this.state.vote_count}|
-              <span className="black-text">Rating: {this.props.review.rating}<br/>
-                {this.props.review.body}</span>
+                <span className="black-text">Rating: {this.props.review.rating}<br/>
+                  {this.props.review.body}</span>
             </div>
           </div>
         </div>
