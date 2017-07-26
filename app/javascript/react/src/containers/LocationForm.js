e9367286210e8d686848c1d034f4dbbf436e93e1
Peter Nelson <peternelson@Peters-MacBook-Air.local>
2017-07-26 09:18:52 -0400

    Added react form validation for locationForm

--- app/javascript/react/src/containers/LocationForm.js
+++ app/javascript/react/src/containers/LocationForm.js
@@ -15,7 +15,7 @@ class LocationForm extends Component {
       state: "",
       zip: "",
       url: "",
-      errorList: []
+      errorObj: {}
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -25,28 +25,117 @@ class LocationForm extends Component {
   }
 
   handleChange(event) {
-    if (!this.errorHandler(event.target.name, event.target.value, errorDictionary)) {
+    if (!this.errorHandler(event.target.name, event.target.value)) {
       this.setState({ [event.target.name]: event.target.value })
+    }
+  }
+
+  errorDictionary(fieldCategory, value) {
+    switch(fieldCategory) {
+      case 'name':
+        return {
+          conditional: (value) => {
+            return (value.length < 2)
+          },
+          message: "name must be a minimum of 2 characters in length"
+        };
+      case 'description':
+        return {
+          conditional: (value) => {
+            return (
+              (value.length <= 50) ||
+              (value.length >= 1000)
+            )
+          },
+          message: "description must be a minimum of 50 characters and no more than 1000 characters in length"
+        };
+      case 'address':
+        return {
+          conditional: (value) => {
+            return (value.length < 2)
+          },
+          message: "address must be a minimum of 2 characters in length"
+        };
+      case 'city':
+        return {
+          conditional: (value) => {
+            return (value.length < 2)
+          },
+          message: "city must be a minimum of 2 characters in length"
+        };
+      case 'zip':
+        return {
+          conditional: (value) => {
+            let regexp = /^[0-9]{5}$/;
+            return (!value.match(regexp))
+          },
+          message: "zip code must be exactly 5 numbers long (and numeric)"
+        };
+      case 'url':
+        return {
+          conditional: (value) => {
+            let regexp = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+            return (
+              (value === '') ||
+              (!value.match(regexp))
+            )
+          },
+          message: "the url must either be a valid url or left blank"
+        };
+      case 'state':
+        return {
+          conditional: (value) => {
+            return (value === '')
+          },
+          message: "please select a state from the dropdown menu"
+        };
+    };
+  };
+
+  errorHandler(fieldCategory, value) {
+    let errorRef = this.errorDictionary(fieldCategory, value);
+
+    if (errorRef.conditional(value)) {
+      let newErrorObj = Object.assign( {}, this.state.errorObj, {[fieldCategory]: errorRef.message} );
+      this.setState({ errorObj: newErrorObj });
+    }
+    else {
+      if (Object.entries(this.state.errorObj).length !== 0) {
+        let newErrorObj = Object.keys(this.state.errorObj)
+          .filter( key => key !== fieldCategory)
+          .reduce( (result, current) => {
+            result[current] = this.state.errorObj[current];
+            return result;
+        }, {});
+
+        this.setState({ errorObj: newErrorObj })
+      }
     }
   }
 
   handleFormSubmit(event) {
     event.preventDefault();
 
-    let formPayload = {
-      name: this.state.name,
-      description: this.state.description,
-      address: this.state.address,
-      city: this.state.city,
-      state: this.state.state,
-      zip: this.state.zip,
-      url: this.state.url
+    debugger;
+
+    if (Object.keys(this.state.errorObj).length === 0) {
+      let formPayload = {
+        name: this.state.name,
+        description: this.state.description,
+        address: this.state.address,
+        city: this.state.city,
+        state: this.state.state,
+        zip: this.state.zip,
+        url: this.state.url
+      }
+
+      console.log(`Payload from within LocationForm: ${formPayload}`)
+
+      this.props.createLocation(formPayload);
+      this.handleClearForm(event);
+    } else {
+      alert('you must complete all forms with the correct formatting before location can be added...there are still Errors in: ' + Object.keys(this.state.errorObj))
     }
-
-    console.log(`Payload from within LocationForm: ${formPayload}`)
-
-    this.props.createLocation(formPayload);
-    this.handleClearForm(event);
   }
 
   handleClearForm(event) {
@@ -59,68 +148,17 @@ class LocationForm extends Component {
       state: "",
       zip: "",
       url: "",
-      errorList: []
+      errorObj: {}
     })
   }
 
-  errorHandler(fieldCategory, value, errorDictionary) {
-    if (errorDictionary.fieldCategory.conditional(value)) {
-      let errorsArray = [...this.state.errorList, errorDictionary.fieldCategory.message]
-      this.setState({ errorList: errorsArray });
-    }
-    else {
-      if (errorArray.includes(message)) {
-        let newError = this.state.errorList.filter((error) => {
-          return error !== errorDictionary.fieldCategory.message
-        })
-        this.setState({ errorList: newError })
-      }
-    }
-  }
 
   render() {
-    let errorDictionary = {
-      name: {
-        conditional: (value) => {
-          return (value.length >= 2)
-        },
-      message: "name must be a minimum of 2 characters in length"
-      },
-      description: {
-        conditional: () => {
-          return (50 <= value.length && value.length <= 1000)
-        },
-        message: "description must be a minimum of 50 characters and no more than 1000 characters in length"
-      },
-      address: {
-        conditional: () => {
-          return (value.length >= 2)
-        },
-        message: "address must be a minimum of 2 characters in length"
-      },
-      city: {
-        conditional: () => {
-          return (value.length >= 2)
-        },
-        message: "city must be a minimum of 2 characters in length"
-      },
-      zip: {
-        conditional: () => {
-          return (value.length === 5 && value.match('/^[0-9]{5}$/'))
-        },
-        message: "zip code must be exactly 5 numbers"
-      },
-      url: {
-        conditional: () => {
-          return value.match('/^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix')
-        },
-        message: "url must be a valid url"
-      }
-    }
-
-    let fieldCategories = Object.keys(this.state).map( (key) => {
-      return key;
-    } ); //array of field categories
+    // if (Object.entries(this.state.errorObj).length !== 0) {
+    //   console.log("state after render: " + Object.keys(this.state.errorObj))
+    // } else {
+    //   console.log("state after render: {}")
+    // }
 
     let states = [
       {"": "Select State"},
@@ -185,21 +223,41 @@ class LocationForm extends Component {
       {"WY": "Wyoming"}
     ]
 
+    let fieldCategories = Object.keys(this.state).map( (key) => {
+      return key
+    }); //array of field categories
+
+    fieldCategories.pop() //removes the errorObj field from the categories array
+
     let textFields = fieldCategories.map( (fieldCategory, index) => {
       let descriptionField;
       let input;
-      if (fieldCategory == "description") {
+      if (fieldCategory === "description") {
+        let errorMessage = ""
+        if(Object.entries(this.state.errorObj).length !== 0) {
+          errorMessage = this.state.errorObj[fieldCategory]
+        }
+
         return(
-          <TextArea
-            key={index}
-            name={fieldCategory}
-            label={fieldCategory}
-            handleChange={this.handleChange}
-            content={this.state[fieldCategory]}
-          />
+          <div key={index}>
+            <p>{errorMessage}</p>
+            <TextArea
+              key={index}
+              name={fieldCategory}
+              label={fieldCategory}
+              handleChange={this.handleChange}
+              content={this.state[fieldCategory]}
+            />
+          </div>
         )
       }
       else if (fieldCategory == "state") {
+        let errorMessage = ""
+        if(Object.entries(this.state.errorObj).length !== 0) {
+          errorMessage = this.state.errorObj[fieldCategory]
+        }
+
+        <p>{errorMessage}</p>
         return(
           <SelectField
             key={index}
@@ -211,32 +269,31 @@ class LocationForm extends Component {
           />
         )
       } else {
+        let errorMessage = ""
+        if(Object.entries(this.state.errorObj).length !== 0) {
+          errorMessage = this.state.errorObj[fieldCategory]
+        }
+
         return(
-          <TextField
-            key={index}
-            name={fieldCategory}
-            handleChange={this.handleChange}
-            content={this.state[fieldCategory]}
-          />
+          <div key={index}>
+            <p>{errorMessage}</p>
+            <TextField
+              key={index}
+              name={fieldCategory}
+              handleChange={this.handleChange}
+              content={this.state[fieldCategory]}
+            />
+          </div>
         )
       }
     })
 
-    let errors = this.state.errorList.map(error => {
-      return(
-        <h5 key={error}>
-        {error}
-        </h5>
-      )
-    })
-
     return(
       <div>
-      {errors}
-      <form className="form" onSubmit={this.handleFormSubmit}>
-        {textFields}
-        <input type="submit" className="btn waves-effect waves-light" name="Submit"/>
-      </form>
+        <form className="form" onSubmit={this.handleFormSubmit}>
+          {textFields}
+          <input type="submit" className="btn waves-effect waves-light" name="Submit"/>
+        </form>
       </div>
     )
   }
