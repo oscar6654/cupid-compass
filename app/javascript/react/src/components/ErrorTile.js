5d0897333ab080b8f47387c4d7272f1af7a6535d
Peter Nelson <peternelson@Peters-MacBook-Air.local>
2017-07-27 13:10:28 -0400

    refactored (fixed) location form validation to work correctly/ look nice

--- app/javascript/react/src/components/ErrorTile.js
+++ app/javascript/react/src/components/ErrorTile.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+
 const ErrorTile = props => {
+  let errorMessage = " "
+  if (props.showError) {
+    errorMessage = props.errorMessage
+  }
+
   return (
     <p className = "error-message">
-      {props.errorMessage}
+      {errorMessage}
     </p>
   );
 }
