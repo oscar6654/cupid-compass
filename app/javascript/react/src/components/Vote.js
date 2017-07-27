9b3fc46b4c3f8b9043a5bbbf9ae4fe83ed532625
ryhornalehach <ryhornalehach@icloud.com>
2017-07-27 13:56:28 -0400

    fixed upvote buttons

--- app/javascript/react/src/components/Vote.js
+++ app/javascript/react/src/components/Vote.js
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Vote = props =>{
   return(
-    <button
-    className="waves-effect waves-teal btn-flat">
+    <button className="waves-effect waves-teal btn-floating white btn-flat">
       <i
-        className="small material-icons"
+        className="small material-icons black-text"
         onClick={props.handleChange}>
         {props.button}
       </i>
