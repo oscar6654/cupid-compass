05b9971729471569b19ee2bec4d53e7ba58b01ef
ryhornalehach <ryhornalehach@icloud.com>
2017-07-27 09:15:05 -0400

    modified search logic to be case insensitive

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,9 @@
 class LocationsController < ApplicationController
   def search
-    query = "%#{params[:query]}%"
+    query = "%#{params[:query].downcase}%"
     @search = query
     @locations = Location
-      .where('name like ? or description like ? or city like ? or state like ?',
+      .where('lower(name) like ? or description like ? or city like ? or state like ?',
              query, query, query, query)
   end
 end
